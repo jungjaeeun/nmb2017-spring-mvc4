eb27a5ab2905701dc34963bcac326c3b8df4f216
jungjaeeun <wodms5772@naver.com>
2017-09-19 11:43:16 +0900

    커밋 & 푸쉬 테스트 코드

---
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -0,0 +1,23 @@
+package kr.hs.emirim.nmb2017.springmvc4.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@Autowired private Twitter twitter;
+	@RequestMapping("/")
+	public String hello(@RequestParam(defaultValue="°í¾çÀÎ") String search, Model model) {
+		SearchResults searchResults = twitter.searchOperations().search(search);
+		String text = searchResults.getTweets().get(0).getText(); 
+		model.addAttribute("message", text);
+		return "resultPage";
+	}
+}
